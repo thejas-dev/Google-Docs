a0bab1128f00061350b396095dc15e4dda55e080
thejas <thejaskala308@gmail.com>
2022-09-04 11:04:09 +0530

    make it better

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,10 +1,8 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
-import {db,storage} from '../../../firebase'
-import { FirestoreAdapter } from "@next-auth/firebase-adapter"
-import * as firestoreFunctions from 'firebase/firestore'
 
-const LOGIN_URL = " http://localhost:3000/api/auth/callback/google"
+
+
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -16,7 +14,7 @@ export default NextAuth({
     }),
     
   ],
-    
+  secret: process.env.JWT_SECRET,
  
   
 })
